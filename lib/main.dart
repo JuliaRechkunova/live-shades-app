f8a9578cae56ec94aebb30b37ee45edf4fba3732
Julia Rechkunova <julia.rechkunova@gmail.com>
2019-03-10 14:15:43 +0100

    Make it dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -28,24 +28,53 @@ class ShadesScreen extends StatefulWidget {
 }
 
 class _ShadesScreenState extends State<ShadesScreen> {
+  ShadesMode _mode = ShadesMode.neutral;
+
+  void _menuItemSelected(ShadesMode mode) {
+    setState(() {
+      _mode = mode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
+          actions: <Widget>[
+            PopupMenuButton(
+              icon: Icon(Icons.palette),
+              itemBuilder: (BuildContext context) {
+                return [
+                  PopupMenuItem(value: ShadesMode.neutral, child: Text('Neutral')),
+                  PopupMenuItem(value: ShadesMode.warmToCold, child: Text('Warm to Cold')),
+                  PopupMenuItem(value: ShadesMode.coldToWarm, child: Text('Cold to Warm')),
+                ];
+              },
+              onSelected: _menuItemSelected,
+            )
+          ],
         ),
         body: Shades(
           baseColor: Colors.deepPurpleAccent,
           count: 5,
+          mode: _mode,
         ));
   }
 }
 
+enum ShadesMode {
+  warmToCold,
+  neutral,
+  coldToWarm
+};
+
 class Shades extends StatelessWidget {
-  Shades({@required this.baseColor, @required this.count});
+  Shades({@required this.baseColor, @required this.count, @required this.mode});
 
   final Color baseColor;
   final int count;
+  final ShadesMode mode;
 
   @override
   Widget build(BuildContext context) {
@@ -54,14 +83,14 @@ class Shades extends StatelessWidget {
 
     List.generate(count, (int index) => index + 1).forEach((int index) {
       Color nextColor = Color(baseColor.value)
-          .withRed(min(baseColor.red + step * index, 255))
+          .withRed(min((baseColor.red + step * index * (mode == ShadesMode.coldToWarm ? 2 : 1)).round(), 255))
           .withGreen(min(baseColor.green + step * index, 255))
-          .withBlue(min(baseColor.blue + step * index, 255));
+          .withBlue(min((baseColor.blue + step * index * (mode == ShadesMode.warmToCold ? 2 : 1)).round(), 255));
 
       Color prevColor = Color(baseColor.value)
-          .withRed(max(baseColor.red - step * index, 0))
+          .withRed(max((baseColor.red - step * index * (mode == ShadesMode.warmToCold ? 0.5 : 1)).round(), 0))
           .withGreen(max(baseColor.green - step * index, 0))
-          .withBlue(max(baseColor.blue - step * index, 0));
+          .withBlue(max((baseColor.blue - step * index * (mode == ShadesMode.coldToWarm ? 0.5 : 1)).round(), 0));
 
       colors.add(nextColor);
       colors.insert(0, prevColor);
