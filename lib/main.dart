f6e1f049ad7e4c89e824cf831a5917546e5e5fcd
Julia Rechkunova <julia.rechkunova@gmail.com>
2019-03-10 19:35:22 +0100

    Slim down

--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,99 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:share/share.dart';
-
 void main() => runApp(MyApp());
-
-final String appTitle = 'Perfect Shades';
-
+final String appName = 'Shading Palette';
+final List<String> shadingModes = ['Neutral', 'Warm to Cold', 'Cold to Warm'];
+enum Mode { neutral, warmToCold, coldToWarm }
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appTitle,
-      theme: ThemeData(primaryColor: Colors.white),
-      home: ShadesScreen(title: appTitle),
-    );
-  }
+  Widget build(BuildContext ctx) => MaterialApp(title: appName, theme: ThemeData(primaryColor: Colors.white), home: Shades());
 }
-
-class ShadesScreen extends StatefulWidget {
-  ShadesScreen({Key key, this.title}) : super(key: key);
-  final String title;
-
+class Shades extends StatefulWidget {
   @override
-  ShadesScreenState createState() => ShadesScreenState();
+  ShadesState createState() => ShadesState();
 }
-
-class ShadesScreenState extends State<ShadesScreen> {
-  Color _baseColor = Colors.deepPurpleAccent;
-  Mode _mode = Mode.neutral;
-  int _count = 5;
-
+class ShadesState extends State<Shades> {
+  final GlobalKey<ScaffoldState> sKey = new GlobalKey<ScaffoldState>();
+  Color baseColor = Colors.deepPurpleAccent;
+  Mode mode = Mode.neutral;
+  int count = 5;
   List<Color> get _colors {
-    List<Color> colors = [_baseColor];
-    int step = (255 / (2 * _count + 1)).floor();
-
-    List.generate(_count, (int index) => index + 1).forEach((int index) {
+    List<Color> colors = [baseColor];
+    int step = (255 / (2 * count + 1)).floor();
+    List.generate(count, (int index) => index + 1).forEach((int index) {
       int value = step * index;
-      Color hColor = Color(_baseColor.value)
-          .withRed(up(_baseColor.red, value, _mode == Mode.coldToWarm))
-          .withGreen(up(_baseColor.green, value, false))
-          .withBlue(up(_baseColor.blue, value, _mode == Mode.warmToCold));
-
-      Color sColor = Color(_baseColor.value)
-          .withRed(down(_baseColor.red, value, _mode == Mode.warmToCold))
-          .withGreen(down(_baseColor.green, value, false))
-          .withBlue(down(_baseColor.blue, value, _mode == Mode.coldToWarm));
-
+      Color hColor = Color(baseColor.value)
+          .withRed(up(baseColor.red, value, mode == Mode.coldToWarm))
+          .withGreen(up(baseColor.green, value, false))
+          .withBlue(up(baseColor.blue, value, mode == Mode.warmToCold));
+      Color sColor = Color(baseColor.value)
+          .withRed(down(baseColor.red, value, mode == Mode.warmToCold))
+          .withGreen(down(baseColor.green, value, false))
+          .withBlue(down(baseColor.blue, value, mode == Mode.coldToWarm));
       colors.add(hColor);
       colors.insert(0, sColor);
     });
-
     return colors;
   }
-
-  int down(int c, int value, bool dominating) =>
-      max((c - value * (dominating ? 0.5 : 1)).round(), 0);
-
-  int up(int c, int value, bool dominating) =>
-      min((c + value * (dominating ? 1.5 : 1)).round(), 255);
-
-  void _modeChanged(Mode mode) {
-    setState(() => _mode = mode);
-  }
-
-  void _colorChanged(Color color) {
-    setState(() => _baseColor = color);
-  }
-
-  void _changeColor(BuildContext context) {
+  int up(int c, int value, bool faster) => min((c + value * (faster ? 1.5 : 1)).round(), 255);
+  int down(int c, int value, bool slower) => max((c - value * (slower ? 0.5 : 1)).round(), 0);
+  String toHex(Color c) => '#${c.value.toRadixString(16).substring(2)}';
+  void modeChanged(Mode m) { setState(() => mode = m); }
+  void colorChanged(Color c) { setState(() => baseColor = c); }
+  void changeColor(BuildContext ctx) {
     showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-              title: Text('Change base color'),
-              content: SingleChildScrollView(
-                  child: ColorPicker(
-                      pickerColor: _baseColor, onColorChanged: _colorChanged)));
-        });
+      context: ctx,
+      builder: (BuildContext _) => AlertDialog(
+        title: Text('Change base color'),
+        content: SingleChildScrollView(child: ColorPicker(pickerColor: baseColor, onColorChanged: colorChanged))));
   }
-
+  void _onTap(Color c) {
+    final snackBar = SnackBar(
+      backgroundColor: Colors.white,
+      content: Text(toHex(c), style: TextStyle(color: Colors.black87)),
+      action: SnackBarAction(label: 'COPY', onPressed: () {Clipboard.setData(ClipboardData(text: toHex(c)));}),
+    );
+    sKey.currentState.hideCurrentSnackBar();
+    sKey.currentState.showSnackBar(snackBar);
+  }
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ctx) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.palette),
-              onPressed: () => _changeColor(context),
-            ),
-            PopupMenuButton(
-              icon: Icon(Icons.tune),
-              itemBuilder: (BuildContext context) {
-                return [
-                  PopupMenuItem(value: Mode.neutral, child: Text('Neutral')),
-                  PopupMenuItem(
-                      value: Mode.warmToCold, child: Text('Warm to Cold')),
-                  PopupMenuItem(
-                      value: Mode.coldToWarm, child: Text('Cold to Warm')),
-                ];
-              },
-              onSelected: _modeChanged,
-            ),
-            IconButton(
-              icon: Icon(Icons.share),
-              onPressed: () {
-                Share.share(_colors
-                    .map((color) =>
-                        '#${color.value.toRadixString(16).substring(2)}')
-                    .join('\n'));
-              },
-            ),
-          ],
-        ),
-        body: ColorList(colors: _colors));
+      key: sKey,
+      appBar: AppBar(
+        title: Text(appName),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.palette), onPressed: () => changeColor(ctx)),
+          PopupMenuButton(
+            icon: Icon(Icons.tune),
+            itemBuilder: (BuildContext _) {
+              return Mode.values
+                .map((Mode m) => PopupMenuItem(
+                  value: m,
+                  child: Text(shadingModes[m.index], style: TextStyle(fontWeight: mode == m ? FontWeight.bold : FontWeight.normal))))
+                .toList();
+            },
+            onSelected: modeChanged,
+          ),
+          IconButton(icon: Icon(Icons.share), onPressed: () {Share.share(_colors.map((c) => toHex(c)).join('\n'));}),
+        ],
+      ),
+      body: Palette(colors: _colors, onTap: _onTap)
+    );
   }
 }
-
-enum Mode { warmToCold, neutral, coldToWarm }
-
-class ColorList extends StatelessWidget {
-  ColorList({@required this.colors});
-
+class Palette extends StatelessWidget {
+  Palette({@required this.colors, @required this.onTap});
   final List<Color> colors;
-
+  final Function onTap;
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: colors
-          .map((color) => Expanded(
-                child: Container(color: color),
-              ))
-          .toList(),
+      children: colors.map((color) => Expanded(child: GestureDetector(onTap: () {onTap(color);}, child: Container(color: color)))).toList(),
     );
   }
 }
