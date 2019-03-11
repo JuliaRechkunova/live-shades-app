5c964459920cc39d9247f6ead3d3d7892019b512
Julia Rechkunova <julia.rechkunova@gmail.com>
2019-03-11 09:07:30 +0100

    Save selected color

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:share/share.dart';
 
 void main() => runApp(App());
@@ -24,7 +25,7 @@ class Shades extends StatefulWidget {
 class ShadesState extends State<Shades> {
   final GlobalKey<ScaffoldState> sKey = new GlobalKey<ScaffoldState>();
 
-  Color baseColor = Colors.deepPurpleAccent;
+  Color baseColor;
   Mode mode = Mode.n;
   int count = 5;
 
@@ -47,24 +48,31 @@ class ShadesState extends State<Shades> {
     return colors;
   }
 
+  @override
+  void initState() { super.initState(); loadColor(); }
+
   int up(int c, int value, bool faster) => min((c + value * (faster ? 1.5 : 1)).round(), 255);
 
   int down(int c, int value, bool slower) => max((c - value * (slower ? 0.5 : 1)).round(), 0);
 
   String toHex(Color c) => '#${c.value.toRadixString(16).substring(2)}';
 
+  void loadColor() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance(); setState(() { baseColor = Color(prefs.getInt('color') ?? Colors.deepPurpleAccent.value); });
+  }
+
   void modeChanged(Mode m) { setState(() => mode = m); }
 
-  void colorChanged(Color c) { setState(() => baseColor = c); }
+  void colorChanged(Color c) async {
+    setState(() => baseColor = c); SharedPreferences prefs = await SharedPreferences.getInstance(); prefs.setInt('color', baseColor.value);
+  }
 
   void countChanged(int c) { setState(() => count = c); }
 
   void changeColor(BuildContext ctx) {
-    showDialog(
-      context: ctx,
-      builder: (BuildContext _) => AlertDialog(
-        title: Text('Change base color'),
-        content: SingleChildScrollView(child: ColorPicker(enableAlpha: false, pickerColor: baseColor, onColorChanged: colorChanged))));
+    showDialog(context: ctx, builder: (BuildContext _) => AlertDialog(
+      title: Text('Change base color'),
+      content: SingleChildScrollView(child: ColorPicker(enableAlpha: false, pickerColor: baseColor, onColorChanged: colorChanged))));
   }
 
   void _onTap(Color c) {
@@ -73,8 +81,7 @@ class ShadesState extends State<Shades> {
       content: Text(toHex(c), style: TextStyle(color: Colors.black87)),
       action: SnackBarAction(label: 'COPY', onPressed: () {Clipboard.setData(ClipboardData(text: toHex(c)));}),
     );
-    sKey.currentState.hideCurrentSnackBar();
-    sKey.currentState.showSnackBar(snackBar);
+    sKey.currentState.hideCurrentSnackBar(); sKey.currentState.showSnackBar(snackBar);
   }
 
   @override
@@ -88,29 +95,23 @@ class ShadesState extends State<Shades> {
           PopupMenuButton(
             icon: Icon(Icons.tune),
             itemBuilder: (BuildContext _) {
-              return Mode.values
-                .map((Mode m) => PopupMenuItem(
-                  value: m,
-                  child: Text(shadingModes[m.index], style: TextStyle(fontWeight: mode == m ? FontWeight.bold : FontWeight.normal))))
-                .toList();
+              return Mode.values.map((Mode m) => PopupMenuItem(value: m,
+                child: Text(shadingModes[m.index], style: TextStyle(fontWeight: mode == m ? FontWeight.bold : FontWeight.normal)))).toList();
             },
             onSelected: modeChanged,
           ),
           PopupMenuButton(
             icon: Icon(Icons.filter_list),
             itemBuilder: (BuildContext _) {
-              return [5, 4, 3, 2, 1]
-                  .map((int c) => PopupMenuItem(
-                  value: c,
-                  child: Text('${c * 2 + 1} shades', style: TextStyle(fontWeight: count == c ? FontWeight.bold : FontWeight.normal))))
-                  .toList();
+              return [5, 4, 3, 2, 1].map((int c) => PopupMenuItem(value: c,
+                child: Text('${c * 2 + 1} shades', style: TextStyle(fontWeight: count == c ? FontWeight.bold : FontWeight.normal)))).toList();
             },
             onSelected: countChanged,
           ),
           IconButton(icon: Icon(Icons.share), onPressed: () {Share.share('$appName (${shadingModes[mode.index]}):\n${colors.map((c) => toHex(c)).join('\n')}');}),
         ],
       ),
-      body: Palette(colors: colors, onTap: _onTap)
+      body: baseColor != null ? Palette(colors: colors, onTap: _onTap) : Center(child: CircularProgressIndicator())
     );
   }
 }
@@ -123,9 +124,6 @@ class Palette extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: colors.map((color) => Expanded(child: GestureDetector(onTap: () {onTap(color);}, child: Container(color: color)))).toList(),
-    );
+    return Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: colors.map((color) => Expanded(child: GestureDetector(onTap: () {onTap(color);}, child: Container(color: color)))).toList(),);
   }
 }
